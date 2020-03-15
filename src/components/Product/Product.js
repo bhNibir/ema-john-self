4c3360996c5677b11f77bf8708a7f5c742ad2b1a
nibir <biplob.asanibir@gmail.com>
2020-03-15 23:29:08 +0600

    add router

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    const {name, img, seller, price, stock} = props.product;
+    const {name, img, seller, price, stock, key} = props.product;
 
     return (
         <div className="product-section">
             <div>
-                <img className="product-img" src={img}></img>
+                <img className="product-img" src={img} alt="product img"></img>
             </div>
             <div className="product-details">
-                <h3>{name}</h3>
+                <h3><Link to={"/product/"+key}>{name}</Link></h3>
                 <p><small>by: {seller} </small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
