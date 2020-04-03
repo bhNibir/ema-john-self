6ad6ee84a8c86edbec157a6262c0c52e2b0c2d7a
nibir <biplob.asanibir@gmail.com>
2020-04-03 12:41:01 +0600

    add shipment

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
 import { useAuth } from '../Login/useAuth';
+import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
 
 const Shipment = () => {
     const { register, handleSubmit} = useForm()
     const auth = useAuth()
     const onSubmit = data => {
-        console.log(data);
+        //TODO: Samad move this after payment
+        console.log(auth.user.email)
+        const saveCart = getDatabaseCart()
+        const productDetails = {user: auth.user.name, cart: saveCart}
+        fetch('http://localhost:4200/placeOrder',{
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(productDetails) // body data type must match "Content-Type" header
+          })
+          .then(res => res.json())
+          .then(data => {
+              console.log("Order Placed")
+              alert("Successfully placed your order")
+              processOrder()
+          })
     }; 
 
     return (
